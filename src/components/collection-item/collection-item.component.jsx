d8585b675d6e6f5cbd2b24a1ac26254b3fcc7af0
Ahmed Mansour <ahmedmmansour18@gmail.com>
2021-07-29 22:12:04 +0200

    update shop data and add cart utils file

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,10 +1,13 @@
 import React from 'react'; 
-import './collection-item.styles.scss';  
+import './collection-item.styles.scss';
 
-
- const  CollectionItem =  ({id , name , price , imageUrl }) =>  {
+import CustomButton from '../custom-button/custom-button.component';
+import { connect } from 'react-redux'; 
+import { addItem } from '../../redux/cart/cart.actions';
+const CollectionItem = ({ item ,  addItem }) => {
+    const { id, name, price, imageUrl } = item; 
     return (
-        <div className='collection-item'>
+        <div className='collection-item' key={id}>
             <div
                 className='image'
                 style={{ backgroundImage: `url(${imageUrl})` }}
@@ -13,9 +16,19 @@ import './collection-item.styles.scss';
                 <span className='name'>  { name }  </span>
                 <span className='price'> { price } </span> 
             </div>
+            <CustomButton
+                onClick={() => addItem(item)}
+                inverted
+            >
+                Add to cart
+            </CustomButton>
             
         </div>
     )
 }
 
-export default CollectionItem ; 
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
+})
+
+export default connect(null , mapDispatchToProps)(CollectionItem)  ; 
--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -4,15 +4,16 @@ import './collection-previewstyles.scss'
 import CollectionItem from './../collection-item/collection-item.component';
 
 
-const CollectionPreview = ({ title , items  } ) => {
+const CollectionPreview = ({ title, items }) => {
+    
     return(
         <div className='collection-preview'> 
             <h1 className='title'> { title.toUpperCase() }  </h1>
             <div className='preview' >
                 {items
                     .filter((item , index) => index < 4 )
-                    .map(({id , ...collectionItemProps }) => (
-                        <CollectionItem key={id} {...collectionItemProps} / >  
+                    .map( item  => (
+                        <CollectionItem key={item.id}  item={item} / >  
                 ))}
             </div>
         </div>  
